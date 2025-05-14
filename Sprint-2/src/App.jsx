8198d82668af92c2716fb3f58fe2a95334d6d65a
saulocs4 <saulolp4@gmail.com>
2025-05-14 16:40:45 -0300

    Aula 14.05

--- Sprint-2/src/App.jsx
+++ Sprint-2/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Carrinho from "./components/Carrinho"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Rotas from "./rotas/Rotas"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+    <Rotas/>
     </>
   )
 }
---
+++ Sprint-2/src/pages/PageCarrinho.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PageCarrinho = () => {
+  return (
+    <Carrinho/>
+  )
+}
+
+export default PageCarrinho
